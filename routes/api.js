fc02f0c4048f3fc868f34749e6e5443f6e2d6541
emily-cheung1 <emilycheung777@gmail.com>
2022-02-06 16:35:36 -0800

    post method

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,27 @@
 'use strict';
 
+
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
 module.exports = function (app) {
+
+  mongoose.connect(process.env['MONGO_URI'], {useNewUrlParser: true, useUnifiedTopology: true});
+
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by: {type: String, required: true},
+    assigned_to: String,
+    status_text: String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  });
+
+  let Issue = mongoose.model('Issue', issueSchema);
+
 
   app.route('/api/issues/:project')
   
@@ -12,6 +33,26 @@ module.exports = function (app) {
     .post(function (req, res){
       let project = req.params.project;
       
+      if (!req.body.issue_title|| !req.body.issue_text|| !req.body.created_by) {
+        return res.json({ error: 'required field(s) missing' });
+      }
+
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      })
+      newIssue.save((err, savedIssue) => {
+        if (!err && savedIssue) {
+          return res.json(savedIssue);
+        }
+      })
     })
     
     .put(function (req, res){
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -4,7 +4,66 @@ const assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
+let id1 = '';
+let id2 = '';
 
 suite('Functional Tests', function() {
-  
-});
+  suite('POST /api/issues/{project} => object with issue data', function() {
+    test('Every field filled in', function(done) {
+      chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: 'Title',
+            issue_text: 'Text',
+            created_by: 'Functional Test-Every field filled in',
+            assigned_to: 'Chai',
+            status_text: 'QA',
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.issue_title, 'Title');
+            assert.equal(res.body.issue_text, 'Text');
+            assert.equal(res.body.created_by, 'Functional Test-Every field filled in');
+            assert.equal(res.body.assigned_to, 'Chai');
+            assert.equal(res.body.status_text, 'QA');
+            assert.equal(res.body.project, 'test');
+            id1 = res.body._id;
+            done();
+          });
+      });
+
+     test('Required fields filled in', function(done) {
+       chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: 'Title',
+            issue_text: 'Text',
+            created_by: 'Functional Test-Every field filled in',
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.issue_title, 'Title');
+            assert.equal(res.body.issue_text, 'Text');
+            assert.equal(res.body.created_by, 'Functional Test-Every field filled in');
+            assert.equal(res.body.assigned_to, '');
+            assert.equal(res.body.status_text, '');
+            assert.equal(res.body.project, 'test');
+            id2 = res.body._id;
+            done();
+          });
+     });
+
+      test('Missing required fields', function(done) {
+        chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: 'Title',
+          })
+          .end(function(err, res){
+            assert.equal(res.body.error, 'required field(s) missing');
+            done();
+          })
+      })
+  })
+
+})
